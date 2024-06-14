482627c620dd4ea89cab9a5057977a2872000bcf
Sahil Chopra <sahilchopra838@gmail.com>
2024-06-14 19:48:41 +0530

    crearted resume app in react

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css';
+import Experience from './components/Experience/Experience';
+import HomePage from './components/HomePage/HomePage';
+import Navbar from './components/Navbar/Navbar';
+import Projects from './components/Projects/Projects';
+import TechStack from './components/TechStack/TechStack';
+
+
+function App() {
+
+//Portfolio website
+
+// 1.Navbar
+//2. HomePage
+//3. Tech Stack
+//4. Experience
+//5. Projects
+  return (
+    <div className='app'>
+        <Navbar/>
+        <HomePage/>
+        <TechStack/>
+        <Experience/>
+        <Projects/>
+
+
+    </div>
+  )
+}
+
+export default App
